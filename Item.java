eec1b4cf84c140e5fc85ee5c03574a90c89089ac
george monaghan <georgemonaghan@student-10-245-162-14.wlan.dur.ac.uk>
2018-01-24 09:49:30 +0000

    First building

---
+++ Item.java
@@ -0,0 +1,9 @@
+public class Item{
+    private String name;
+    private double pricePreVat;
+    private double VAT;
+    
+    public Item(String name, ){
+        this.name=name;
+        
+    }
